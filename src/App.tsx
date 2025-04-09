37738faa8e705f023d980655846b3d39112f2eda
ABHISHEK <abhishekp9a@gmail.com>
2025-04-09 22:43:09 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewYorkTimesHomepage from './components/NewYorkTimesHomepage';
+
+function App() {
+  return (
+    <div className="App">
+      <NewYorkTimesHomepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}",  // Add your React files path here
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
